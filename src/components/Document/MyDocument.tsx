b96f39b62c64336990922fb5dd7ebc6ea332e528
chmonka <barabashka040504@gmail.com>
2024-05-07 17:23:36 +0300

    превью пдф

--- src/components/Document/MyDocument.tsx
+++ src/components/Document/MyDocument.tsx
@@ -21,7 +21,7 @@ const MyDocument = () => {
         const pdf = new jsPDF('portrait', 'pt', 'a4')
         // const pdfWidth = pdf.internal.pageSize.getWidth()
         // const pdfHeight = pdf.internal.pageSize.getWidth()
-        pdf.addImage(imgData, 'SVG', 0, 0, 600, 50)
+        pdf.addImage(imgData, 'SVG', 0, 0, 600, 900)
         pdf.save('shipping_label.pdf')
       })
     } else {
@@ -32,133 +32,166 @@ const MyDocument = () => {
   return (
     <Box sx={{
       display: 'flex',
-      flexDirection: 'column',
-      height: '100%',
-      width: '100%',
-      border: '1px solid black',
-      alignItems: 'center',
-      minWidth: '795px',
-      minHeight: '900px',
-      maxWidth: '795px',
-      maxHeight: '1000px',
-      justifyContent: 'center',
-      borderRadius: '4px',
+      maxWidth: '100%',
+      width: '800px',
+      height: '1000px',
+      maxHeight: '100%',
+      border: '1px solid black'
     }}>
-      <Grid
-        sx={{
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        width:'700px'
+      
+      }}>
+        <Box sx={{
           display: 'flex',
-          flexDirection: 'column',
-          width: '100%',
-          height: '100%',
-          minWidth: '595px',
-          minHeight: '800px',
-          maxWidth: '595px',
-          maxHeight: '800px',
-          border: '1px solid black',
-          borderRadius: '4px',
+          width: '210mm',
+          height: '297mm',
         }}>
-        <Grid
-          ref={pdfRef}
-          sx={{
-            display: 'flex',
-          }}>
-          <Box
-            sx={{backgroundColor: '#023e8a',
-              minHeight: '800px',
-              maxHeight: '800px', width: '100%'}}>
-            <Typography>Желаемая зарплата:</Typography>
-            <Box>
-              <Typography>{object.money} {object.currency}</Typography>
-            </Box>
-
-          </Box>
-        </Grid>
-
-      </Grid>
-      <button onClick={downloadPDF} style={{ fontSize: '10px' }}>
-        Download
-      </button>
-
-      {/*  <Box
-        sx={{}}
-      >
-        <Grid
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%',
-            height: '100%',
-            minWidth: '795px',
-            minHeight: '1000px',
-            maxWidth: '795px',
-            maxHeight: '1000px',
-            borderRadius: '4px',
-            boxShadow: '0 0 32px white',
-            overflow: 'hidden',
-          }}>
           <Box
             sx={{
-              textAlign: 'left',
-              position: 'relative',
-              margin: 'auto',
-              width: '795px',
+              height: '800px',
+              width: '40%',
+              background: '#023e8a'   
             }}>
-            <Grid
-              ref={pdfRef}
-              sx={{
-                width: '210mm',
-                height: '297mm',
-                // maxHeight: '297mm',
-                display: 'flex',
-              }}>
-              <Box
-                sx={{
-                  padding: '20px',
-                  height: '100%',
-                  width: '40%',
-                  position: 'relative',
-                  backgroundColor: '#023e8a',
-                }}>
-                <Box>
-                  <Typography>Желаемая зарплата:</Typography>
-                  <Box>
-                    <Typography>{object.money} {object.currency}</Typography>
-                  </Box>
-                </Box>
-                <Box>
-                  <Typography>Гражданство:</Typography>
-                  <Box>
-                    <Typography>{object.citizenship}</Typography>
-                  </Box>
-                </Box>
-              </Box>
-              <Box
-                sx={{
-                  backgroundColor: '#03045e',
-                  height: '100%',
-                  width: '60%',
-                  position: 'relative',
-                  padding: '20px',
-                }}
-              >
-                <Box>
-                  <Typography
-                    variant="h6">{object.middleName.toUpperCase()} {object.firstName.toUpperCase()} {object.lastName.toUpperCase()}</Typography>
-                  <Typography variant="h6">{object.desiredPosition}</Typography>
-                  <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                    <PlaceIcon sx={{ color: 'rgba(255, 255, 255, 1)' }} />
-                    <Typography>{object.city}</Typography>
-                  </Box>
-                </Box>
-              </Box>
-            </Grid>
+            <Typography>{object.money} {object.currency}</Typography>
           </Box>
-        </Grid>
+          <Box
+            sx={{
+              height: '800px',
+              width: '100%',
+              background: 'red'
+            }}
+          >
+            <Typography>{object.money} {object.currency}</Typography>
+          </Box>
+        </Box>
       </Box>
-      <Box>
-
-      </Box>*/}
     </Box>
+
+
+    /* <Grid
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        height: '100%',
+        minWidth: '600px',
+        minHeight: '800px',
+        maxWidth: '600px',
+        maxHeight: '800px',
+        border: '1px solid black',
+        borderRadius: '4px',
+      }}>
+      <Grid
+        ref={pdfRef}
+        sx={{
+          display: 'flex',
+        }}>
+        <Box
+          sx={{
+            backgroundColor: '#023e8a',
+            height: '800px', width: '40%'
+          }}>
+ 
+
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: 'red',
+            width: '100%',
+            height: '800px',
+          }}
+        >
+          <Typography>{object.money} {object.currency}</Typography>
+        </Box>
+      </Grid>
+
+    </Grid>
+    <button onClick={downloadPDF} style={{ fontSize: '10px' }}>
+      Download
+    </button> */
+
+    /*  <Box
+          sx={{}}
+        >
+          <Grid
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              height: '100%',
+              minWidth: '795px',
+              minHeight: '1000px',
+              maxWidth: '795px',
+              maxHeight: '1000px',
+              borderRadius: '4px',
+              boxShadow: '0 0 32px white',
+              overflow: 'hidden',
+            }}>
+            <Box
+              sx={{
+                textAlign: 'left',
+                position: 'relative',
+                margin: 'auto',
+                width: '795px',
+              }}>
+              <Grid
+                ref={pdfRef}
+                sx={{
+                  width: '210mm',
+                  height: '297mm',
+                  // maxHeight: '297mm',
+                  display: 'flex',
+                }}>
+                <Box
+                  sx={{
+                    padding: '20px',
+                    height: '100%',
+                    width: '40%',
+                    position: 'relative',
+                    backgroundColor: '#023e8a',
+                  }}>
+                  <Box>
+                    <Typography>Желаемая зарплата:</Typography>
+                    <Box>
+                      <Typography>{object.money} {object.currency}</Typography>
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Typography>Гражданство:</Typography>
+                    <Box>
+                      <Typography>{object.citizenship}</Typography>
+                    </Box>
+                  </Box>
+                </Box>
+                <Box
+                  sx={{
+                    backgroundColor: '#03045e',
+                    height: '100%',
+                    width: '60%',
+                    position: 'relative',
+                    padding: '20px',
+                  }}
+                >
+                  <Box>
+                    <Typography
+                      variant="h6">{object.middleName.toUpperCase()} {object.firstName.toUpperCase()} {object.lastName.toUpperCase()}</Typography>
+                    <Typography variant="h6">{object.desiredPosition}</Typography>
+                    <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                      <PlaceIcon sx={{ color: 'rgba(255, 255, 255, 1)' }} />
+                      <Typography>{object.city}</Typography>
+                    </Box>
+                  </Box>
+                </Box>
+              </Grid>
+            </Box>
+          </Grid>
+        </Box>
+        <Box>
+  
+        </Box>*/
   )
 }
 
